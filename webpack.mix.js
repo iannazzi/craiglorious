cc10c636fc0de851a390b521da6918024ebf4c9a
vagrant <vagrant@homestead>
2018-01-12 01:19:15 +0000

    udated migration

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,9 +3,9 @@ process.env.DISABLE_NOTIFIER = true
 let mix = require('laravel-mix');
 
 
-mix.js('src/app.js', 'public/js');
-mix.sass('src/sass/app.scss', 'public/css');
-mix.js('src/server_connection.js', 'public/js/server_connection.js');
+mix.js('src/app.js', 'public/js').version();
+mix.sass('src/sass/app.scss', 'public/css').version();
+mix.js('src/server_connection.js', 'public/js/server_connection.js').version();
 
 mix.copy('node_modules/font-awesome/fonts/', 'public/fonts/');
 mix.copy('node_modules/fullcalendar/dist/fullcalendar.min.css', 'public/css/');
