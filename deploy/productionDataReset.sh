8d56040aae35314677dd46766b3e081362e1c704
Craig Iannazzi <ci@embrasse-moi.com>
2018-01-22 13:36:34 -0500

    views and production data

--- deploy/productionDataReset.sh
+++ deploy/productionDataReset.sh
@@ -2,14 +2,14 @@
 f=/var/www/craiglorious.com
 
 
-read -p "Are you sure? " -n 1 -r
+read -p "THIS WILL KILL PRODUCTION DATA Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    sed s/APP_ENV=production/APP_ENV=staging/g $f/live/.env
+    sed -i 's/APP_ENV=production/APP_ENV=staging/g' $f/live/.env
     cd $f/live
     php artisan zz:dms
     php artisan zz:dst
-    sed s/APP_ENV=staging/APP_ENV=production/g $f/live/.env
+    sed -i 's/APP_ENV=staging/APP_ENV=production/g' $f/live/.env
 fi
 
