d673c2cbbb4e103f332a8234d8a6af774c55e08a
Craig Iannazzi <ci@embrasse-moi.com>
2018-02-05 12:17:17 -0500

    getting ready to seed EM

--- deploy/scpScripts.sh
+++ deploy/scpScripts.sh
@@ -2,5 +2,6 @@
 
 #copy the script files over to the production server
 rsync -avzh -e ssh *.sh craig@craiglorious.com:/var/www/craiglorious.com/scripts/
+rsync -avzh -e ssh ../database/seeds/tenant/EmbrasseMoi/DataGitIgnore/* craig@craiglorious.com:/var/www/craiglorious.com/data/EmbrasseMoi
 
 
